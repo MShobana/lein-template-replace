731c6a566ed2edaaecdf11103281e859ea0d9f58
Shobana <shobanam@thoughtworks.com>
2017-08-31 08:47:21 +0000

    Updated the plugin url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject template-replace "0.1.0-SNAPSHOT"
   :description "Leiningen plugin to replace templates in given files with the given value and output into the target files"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/MShobana/lein-template-replace"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[selmer "1.0.2"]]
